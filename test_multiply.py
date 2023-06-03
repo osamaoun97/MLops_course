a5523bc9dfc67a481435f5dfabe1498e61626ce2
Osama Fayez <osamaoun997@gmail.com>
2023-06-03 13:16:18 +0300

    test func 4

--- test_multiply.py
+++ test_multiply.py
@@ -1,7 +1,9 @@
+"""test module"""
 from multiply import multiply
 
 
 def test_multiply():
+    '''test function'''
     assert 6 == multiply(3, 2)
     assert 4 == multiply(2, 2)
     assert 12 == multiply(2, 6)
