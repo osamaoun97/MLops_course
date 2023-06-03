7fdcdbc1e29e802e9ac52fd75b3053d89942a8d6
Osama Fayez <osamaoun997@gmail.com>
2023-06-03 10:53:19 +0300

    init repo

---
+++ multiply.py
@@ -0,0 +1,8 @@
+"""hello module"""
+
+def multiply(var1, var2):
+    """multiply function"""
+    return var1 * var2
+    
+if __name__ == "__main__":
+    print(multiply(3, 2))
---
+++ test_multiply.py
@@ -0,0 +1,6 @@
+from multiply import multiply
+
+
+def test_multiply():
+    assert 6 == multiply(3, 2)
+    assert 4 == multiply(2, 2)
